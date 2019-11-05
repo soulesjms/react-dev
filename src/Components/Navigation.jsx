3d334f146488d8c75b029286ad22ff19f9c890b5
Soules <jmsoule@evoforge.org>
2019-11-05 16:45:37 -0500

    initial commit

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          GS Software
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
